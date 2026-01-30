ab2ff38167572acff39df94bb8087a13b9a8afdb
joaquintides <joaquin@tid.es>
2026-01-30 19:52:49 +0100

    updated to container new location

--- test/cmake_test/main.cpp
+++ test/cmake_test/main.cpp
@@ -1,14 +1,14 @@
-/* Copyright 2025 Joaquin M Lopez Munoz.
+/* Copyright 2025-2026 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  */
 
-#include <boost/hub/hub.hpp>
+#include <boost/container/hub.hpp>
 
 int main()
 {
-  boost::hub<int> x;
+  boost::container::hub<int> x;
   x.insert(0);
 
   return 0;
