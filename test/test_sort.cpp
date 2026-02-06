98f7602d05ad23cf48acc3db8197868b9cfedafc
joaquintides <joaquin@tid.es>
2026-02-06 12:41:52 +0100

    extended previous workaround

--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -33,8 +33,9 @@ static_assert(
   !std::is_trivially_destructible<big_nontrivial_int>::value &&
 #if !BOOST_WORKAROUND(BOOST_LIBSTDCXX_VERSION, < 50000)
   !std::is_trivially_copy_constructible<big_nontrivial_int>::value &&
+  !std::is_trivially_assignable<big_nontrivial_int, big_nontrivial_int>::value
 #endif
-  !std::is_trivially_assignable<big_nontrivial_int, big_nontrivial_int>::value,
+  ,
   "internal check on big_nontrivial_int");
 
 template<typename Hub, typename Compare = std::less<typename Hub::value_type>>
