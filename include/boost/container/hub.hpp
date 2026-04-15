299737a01605f70d4f0c2b42f30d4a8488625a81
joaquintides <joaquin@tid.es>
2026-04-15 15:57:38 +0200

    reverted code commits to investigate gcovr vehavior

--- include/boost/container/hub.hpp
+++ include/boost/container/hub.hpp
@@ -1065,9 +1065,8 @@ public:
     /* Linear on # available blocks, std::hive is linear on # _reserved_
      * blocks.
      */
-    if(capacity() <= n) return;
     for(auto pbb = blist.header()->next_available;
-        capacity() - n >= N && pbb != blist.header(); ) {
+        capacity() > n && pbb != blist.header(); ) {
       auto pb = static_cast_block_pointer(pbb);
       pbb = pbb-> next_available;
       if(pb->mask == 0) {
