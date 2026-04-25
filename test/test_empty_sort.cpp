b89e3ad459e26811b7c0bdc48dcdeee37240b266
joaquintides <joaquin@tid.es>
2026-04-25 13:03:48 +0200

    worked around bogus array bounds warning in GCC (Andrzej Krzemieński/Peter Dimov) (#12)
    * exposed warning on empty sort
    * previous didn't expose the warning, reformulated
    * didn't work either, isolated in its own TU
    * worked around warning

---
+++ test/test_empty_sort.cpp
@@ -0,0 +1,22 @@
+/* Copyright 2026 Joaquin M Lopez Munoz.
+ * Distributed under the Boost Software License, Version 1.0.
+ * (See accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <boost/container/hub.hpp>
+#include <boost/core/lightweight_test.hpp>
+
+
+int main()
+{
+  /* Warning reported in 
+   * https://lists.boost.org/archives/list/boost@lists.boost.org/
+   * message/ZTL2D2UMIHAAC6ZLHLH4Y3XETTQXAHBZ/ .
+   */
+  boost::container::hub<int> h{};
+  h.sort();
+  BOOST_TEST(h.empty());
+
+  return boost::report_errors();
+}
